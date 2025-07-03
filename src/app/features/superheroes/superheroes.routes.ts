ac1e91bdc6574c86010104b8a43108c967392380
Momfus <momfus@outlook.com>
2025-07-03 18:06:05 -0300

    adding short path tsconfig

--- src/app/features/superheroes/superheroes.routes.ts
+++ src/app/features/superheroes/superheroes.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from "@angular/router";
-import { PageNotFoundComponent } from "../../shared/components/page-not-found/page-not-found.component";
 import { SuperheroesLayoutComponent } from "./layouts/superheroes-layout/superheroes-layout.component";
-import { SuperheroDetailComponent } from "./pages/superhero-detail/superhero-detail.component";
 import { SuperheroListComponent } from "./pages/superhero-list/superhero-list.component";
+import { SuperheroDetailComponent } from "./pages/superhero-detail/superhero-detail.component";
+import { PageNotFoundComponent } from "@shared/components/page-not-found/page-not-found.component";
 
 
 export const superHeroesRoutes: Routes = [
